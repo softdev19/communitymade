2e68f9bde6a75c5a11fe57f14cbc54c2d8c845ac
collinrentz7 <collinrentz2@gmail.com>
2021-03-02 16:06:39 +0200

    finished UI Settings screen

---
+++ src/components/organisms/Tabs.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
+import platform from '../../helpers/platform'
+import { useTranslation } from 'react-i18next'
+
+function Tabs({ children, activeTab, setActiveTab }) {
+  const { t } = useTranslation('settings')
+  return(
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <TouchableOpacity
+          style={[
+            styles.btn,
+            activeTab === 0 && styles.btnActive,
+            styles.firstBtn
+          ]}
+          onPress={() => setActiveTab(0)}
+        >
+          <Text>{t('skills')}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.btn, activeTab === 1 && styles.btnActive]}
+          onPress={() => setActiveTab(1)}
+        >
+          <Text>{t('address')}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.btn, activeTab === 2 && styles.btnActive]}
+          onPress={() => setActiveTab(2)}
+        >
+          <Text>{t('bank info')}</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.block}>
+        {children}
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: 20
+  },
+  row: {
+    flexDirection: 'row',
+    height: 30,
+    paddingLeft: 50,
+    bottom: -1,
+    zIndex: 1
+  },
+  btn: {
+    borderWidth: 1,
+    backgroundColor: platform.defaultBackgroundColor,
+    paddingHorizontal: 5,
+    borderLeftWidth: 0
+  },
+  firstBtn: {
+    borderLeftWidth: 1
+  },
+  btnActive: {
+    borderBottomWidth: 0
+  },
+  block: {
+    width: platform.deviceWidth - 32,
+    flex: 1,
+    alignSelf: 'center',
+    borderWidth: 1
+  }
+})
+
+export default Tabs
--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -4,12 +4,67 @@ import { connect } from 'react-redux'
 import { useTranslation } from 'react-i18next'
 import platform from '../helpers/platform'
 import _ from 'lodash'
+import { Button, Checkbox } from 'react-native-material-ui'
+
+import CompanyTitle from '../components/molecules/CompanyTitle'
+import ScreenTitle from '../components/molecules/ScreenTitle'
+import Spacer from '../components/atoms/Spacer'
+import Tabs from '../components/organisms/Tabs'
 
 function SettingsScreen({ navigation }) {
   const { t } = useTranslation(['settings', 'common'])
+  const [activeTab, setActiveTab] = useState(0)
+  const [skills, setSkills] = useState([])
+  const onPressContinueSkills = () => {
+
+  }
+  const data = ['Sewing', 'Assembling', 'Tailoring', 'Warehousing', 'Shipping']
   return (
     <View style={styles.container}>
-
+      <View style={styles.header} />
+      <CompanyTitle />
+      <Spacer />
+      <ScreenTitle title={t('settings')} />
+      <Tabs activeTab={activeTab} setActiveTab={setActiveTab}>
+        {activeTab === 0 && (
+          <>
+            <Spacer />
+            <ScreenTitle title={t('choose skills')} />
+            <Spacer />
+            {_.map(data, (item, key) => {
+              const checkedIndex = _.indexOf(skills, item)
+              return (
+                <View style={styles.checkbox} key={key}>
+                  <Checkbox
+                    label={item}
+                    value={item}
+                    checked={checkedIndex > -1}
+                    onCheck={bool => {
+                      if (checkedIndex > -1) {
+                        const newSkills = [
+                          ...skills.slice(0, checkedIndex),
+                          ...skills.slice(checkedIndex + 1)
+                        ]
+                        setSkills(newSkills)
+                      } else {
+                        setSkills([...skills, item])
+                      }
+                    }}
+                  />
+                </View>
+              )
+            })}
+            <Spacer />
+            <Button
+              raised
+              style={{ container: styles.btn }}
+              text={t('common:submit')}
+              disabled={_.size(skills) === 0}
+              onPress={onPressContinueSkills}
+            />
+          </>
+        )}
+      </Tabs>
     </View>
   )
 }
@@ -17,6 +72,19 @@ function SettingsScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  header: {
+    height: platform.topSpace + 20
+  },
+  btn: {
+    width: 100,
+    height: 30,
+    alignSelf: 'center'
+  },
+  checkbox: {
+    width: 160,
+    height: 40,
+    alignSelf: 'center'
   }
 })
 
